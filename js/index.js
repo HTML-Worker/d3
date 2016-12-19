580a753ef83009e0a4e62647a2981d554fcaee25
HTML-Worker <baolong@gridsum.com>
2016-12-19 17:51:55 +0800

    添加了增加按钮

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
-var dataset=[50,43,120,87,99,167,142];
+var dataset = [50,43,120,87,99,167,142];
 
-d3.select("body").selectAll("div")
-    .data(dataset)
-    .enter()
-    .append("div")
-    .classed("bar",true)
-    .style("height",function(d) {
-        return d + "px";
-    });
+function myadd() {
+    d3.select("body").selectAll("div")
+        .data(dataset)
+        .enter()
+        .append("div")
+        .classed("bar", true)
+        .style("height", function(d) {
+            return d + "px";
+        })
+}
