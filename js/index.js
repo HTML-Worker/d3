767abacd7872a667febf64431b706df9ffc2e1a6
HTML-Worker <baolong@gridsum.com>
2016-12-22 15:31:20 +0800

    优化完成

--- js/index.js
+++ js/index.js
@@ -1,17 +1,30 @@
 var data = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
         11, 12, 15, 20, 18, 17, 16, 18, 23, 25];
 
-function rectBuild (data, dom) {
+var setting = {
+    style: {
+        xFontSize : 11,
+        yFontSize : 9,
+        rectClass : "bar",
+        xAxisClass : "xAxis",
+        yAxisClass : "yAxis",
+        fontFamily : "sans-serif",
+        textFontSize : 11,
+        textFill : "white"
+    },
+    color: ['#c3d8f0', '#aac7e8', '#6796ca', '#5588c1','#497ebb', '#3878c1', '#2b6cb8', '#1c5fad','#0f529f', '#064a98']
+};
+
+function rectBuild (data, dom, setting) {
     this.data = data;
     this.dom = dom;
-    var obj = {};
     var useData = {
+        m : 0,
         w : 600,
         h : 160,
         padding : 20,
         paddingLeft : 10
     };
-
     //创建svg模型对象
     var svg = d3.select(this.dom)
         .append("svg")
@@ -41,12 +54,34 @@ function rectBuild (data, dom) {
         .tickSize(0, 0)
         .ticks(5);
 
-    obj.build = function() {
+    //颜色处理方法
+    colorChoose = function (d, i) {
+        var chooseColor;
+        if(setting.color) {
+            if(setting.color[i] != null) {
+                chooseColor = setting.color[i];
+            }else {
+                if(useData.m < setting.color.length) {
+                    chooseColor = setting.color[useData.m];
+                    useData.m++;
+                }else {
+                    useData.m = 0;
+                    chooseColor = setting.color[useData.m];
+                    useData.m++;
+                }
+            }
+        }else {
+            chooseColor = "rgb(56, 193, " + (d * 10) + ")";
+        }
+        return chooseColor;
+    };
+
+    this.build = function () {
         svg.selectAll("rect")
             .data(data)
             .enter()
             .append("rect")
-            .classed("bar", true)
+            .classed(setting.style.rectClass, true)
             .attr("x", function(d, i) {
                 return xScale(i) + useData.paddingLeft;
             })
@@ -57,8 +92,8 @@ function rectBuild (data, dom) {
             .attr("height", function (d) {
                 return useData.h - yScale(d);
             })
-            .attr("fill", function (d) {
-                return "rgb(56, 193, " + (d * 10) + ")";
+            .attr("fill", function (d, i) {
+                return colorChoose(d, i);
             });
 
         svg.selectAll("text")
@@ -75,26 +110,25 @@ function rectBuild (data, dom) {
             .attr("y", function (d) {
                 return yScale(d) - useData.padding + 13;
             })
-            .attr("font-family", "sans-serif")
-            .attr("font-size", "11px")
-            .attr("fill", "white");
+            .attr("font-family", setting.style.fontFamily + "px")
+            .attr("font-size", setting.style.textFontSize + "px")
+            .attr("fill", setting.style.textFill);
 
         //调用数轴函数
         svg.append("g")
-            .classed("xAxis", true)
+            .classed(setting.style.xAxisClass, true)
             .attr("transform", "translate("+ useData.paddingLeft +","+ (useData.h - useData.padding) +")")
-            .attr("font-size", "11px")
+            .attr("font-size", setting.style.xFontSize+"px")
             .call(xAxis);
         svg.append("g")
-            .classed("xAxis", true)
-            .attr("transform", "translate("+ (useData.paddingLeft + xScale(0)) +","+ (-useData.padding) +")")
-            .attr("font-size", "9px")
+            .classed(setting.style.yAxisClass, true)
+            .attr("transform", "translate("+ (useData.paddingLeft + xScale(0)) +","+ ( - useData.padding) +")")
+            .attr("font-size", setting.style.yFontSize+"px")
             .call(yAxis);
     };
-    return obj;
 }
 
-var a = rectBuild(data, "body");
+var a = new rectBuild(data, "body", setting);
 a.build();
 
 
